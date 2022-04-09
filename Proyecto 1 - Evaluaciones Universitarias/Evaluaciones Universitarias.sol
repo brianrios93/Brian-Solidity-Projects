9f4e3fa1c69f2343653d5b307f62ed5d5c72dd66
Brian Rios <70611500+brianrios93@users.noreply.github.com>
2022-04-09 16:57:10 -0300

    Update Evaluaciones Universitarias.sol

--- Proyecto 1 - Evaluaciones Universitarias/Evaluaciones Universitarias.sol
+++ Proyecto 1 - Evaluaciones Universitarias/Evaluaciones Universitarias.sol
@@ -1,3 +1,10 @@
+/* Enunciado: crear un sistema de evaluaciones universitarias, en el que un profesor pueda publicar las notas de las 
+evaluaciones de sus alumnos y ellos puedan visualizarlas. También cada alumno puede solicitar la revisión de su examen
+ingresando su ID en caso de no estar satisfecho con el resultado de su examen, y quedar anotado en una lista. Luego el 
+profesor podrá ver la lista de los alumnos anotados para revisión y cambiar la nota a quienes considere. De esa forma
+actualiza los resultados y podrán ser vistas por los alumnos.
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.4 < 0.7.0;
 pragma experimental ABIEncoderV2;
