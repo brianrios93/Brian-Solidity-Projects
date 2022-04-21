98f3555ccd07ee7e50b7b025bce896d5e59056f5
Brian Rios <70611500+brianrios93@users.noreply.github.com>
2022-04-21 13:37:39 -0300

    Update Lotería.sol

--- Proyecto 4 - Lotería/Lotería.sol
+++ Proyecto 4 - Lotería/Lotería.sol
@@ -5,6 +5,8 @@ ganador. Para ello los clientes pagarán cada boleto con tokens ERC20 generados 
 pagando con Ethers. Cada vez que un cliente compra un boleto, los tokens irán a la dirección del owner del contrato,
 que servirá como caja acumuladora para luego entregar como premio al ganador del sorteo.
 La caja de tokens acumulados se hará en la cuenta del owner.
+Este contrato tiene una opción para consultar la dirección del contrato del token creado (con nombre DSN) para poder
+visualizarlo en la BSCScan, Metamask o donde se lo prefiera.
 
 PD: la ventaja de este sistema es que al estar implementado en la blockchain, se elimina la posibilidad de fraude por parte
 de los organizadores ya que los registros de jugadores de la lotería serán totalmente públicos.
